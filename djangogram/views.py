58f422a648a549cad70477a9fb678df48e59a3c3
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2022-09-14 12:56:27 +0300

    1.2.0

--- djangogram/views.py
+++ djangogram/views.py
@@ -135,6 +135,33 @@ def like_post(request):
         post_id = request.POST.get('post_id')
         post_obj = Post.objects.get(pk=post_id)
 
+        #other method
+        # profile = Profile.objects.get(user=user)
+
+        # if profile in post_obj.liked.all():
+        #     post_obj.liked.remove(profile)
+        # else:
+        #     post_obj.liked.add(profile)
+        #
+        # like, created = Like.objects.get_or_create(user=profile, post_id=post_id)
+        #
+        # if not created:
+        #     if like.value == 'Like':
+        #         like.value = 'Unlike'
+        #     else:
+        #         like.value = 'Like'
+        # else:
+        #     like.value = 'Like'
+        #
+        #     post_obj.save()
+        #     like.save()
+
+        # data = {
+        #     'value': like.value,
+        #     'likes': post_obj.liked.all().count()
+        # }
+        # return JsonResponse(data, safe=False)
+
         if user in post_obj.liked.all():
             post_obj.liked.remove(user)
         else:
@@ -149,6 +176,12 @@ def like_post(request):
                 like.value = 'Like'
 
         like.save()
+        data = {
+            'value': like.value,
+            'likes': post_obj.liked.all().count()
+        }
+        return JsonResponse(data, safe=False)
+
     return redirect('home')
 
 
